6ef6fb5a9b13d3f9a0b971c3645490bad76df94c
Hassan Almas <halmas@ice.digitalbazaar.com>
2015-03-20 13:46:02 -0400

    Add README file and readability changes

--- basic-rest-api/index.js
+++ basic-rest-api/index.js
@@ -10,11 +10,11 @@ bedrock.config.mongodb.port = 27017;            // default: 27017
 bedrock.config.mongodb.username = 'bedrock_rest'; // default: bedrock
 bedrock.config.mongodb.password = 'password';   // default: password
 
-// the mongodb database 'my_project_dev' and the 'my_project' user will
+// the mongodb database 'bedrock_rest_dev' and the 'bedrock_rest' user will
 // be created on start up following a prompt for the admin user credentials
 
 bedrock.events.on('bedrock-mongodb.ready', function(callback) {
-  database.openCollections(['users'], function(err) {
+  database.openCollections(['people'], function(err) {
     if(err) {
       return callback(err);
     }
@@ -24,16 +24,20 @@ bedrock.events.on('bedrock-mongodb.ready', function(callback) {
 
 bedrock.events.on('bedrock-express.configure.routes', function(app) {
   app.get('/users', function(req, res){
-    database.collections.users.find({}).toArray(function(err, docs){
+    database.collections.people.find({}).toArray(function(err, docs){
       res.send(docs);
     });
-  }).post('/users/:name', function(req, res){
-    database.collections.users.insert([{name: req.param("name")}],
+  });
+  
+  app.post('/users/:name', function(req, res){
+    database.collections.people.insert([{name: req.param("name")}],
      function(err, result){
       res.send(result.result);
     });
-  }).delete('/users/:name', function(req, res){
-    database.collections.users.remove({name: req.param("name")},
+  });
+
+  app.delete('/users/:name', function(req, res){
+    database.collections.people.remove({name: req.param("name")},
       function(err, result){
         res.send(result.result);
     });
