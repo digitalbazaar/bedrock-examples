307d9967703f69ca789219a7b28b91b7a2423d1c
Amit Ishairzay <amit.ishairzay@gmail.com>
2015-03-26 16:26:21 -0400

    Add controllers for the different modals

--- basic-modals-example/example/example.js
+++ basic-modals-example/example/example.js
@@ -23,6 +23,49 @@ module.config(function($routeProvider) {
   });
 });
 
+/* @ngInject */
+module.controller('PersonController', function($scope) {
+  $scope.person = {name:"Bedrock", traits:['versatile', 'fast']};
+  //$scope.name = "Bedrock";
+  //$scope.traits = ['versatile', 'fast'];
+  console.log('controller created');
+
+});
+
+module.controller('TraitController', function($scope) {
+  $scope.trait = '';
+});
+
+module.directive('personEditor', function(){
+  return{
+    restrict: 'E',
+    require: '^stackable',
+    templateUrl: 'person-editor',
+    controller: 'PersonController',
+    controllerAs: 'model',
+    link: function(scope, element, attrs, stackable) {
+      scope.ok = function() {
+        stackable.close(scope.person);
+      };
+    }
+  };
+});
+
+module.directive('traitAdder', function(){
+  return{
+    restrict: 'E',
+    require: '^stackable',
+    templateUrl: 'trait-adder',
+    controller: 'TraitController',
+    controllerAs: 'model',
+    link: function(scope, element, attrs, stackable) {
+      scope.ok = function() {
+        stackable.close(scope.trait);
+      };
+    }
+  };
+});
+
 return module.name;
 
 });
