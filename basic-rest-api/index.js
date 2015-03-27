bc73cb62b51449d48e565c31d27fd232c0fe03e1
Dave Longley <dlongley@digitalbazaar.com>
2015-03-27 14:53:53 -0400

    Code style clean up; use "people" consistently.

--- basic-rest-api/index.js
+++ basic-rest-api/index.js
@@ -1,14 +1,15 @@
-// modules
 var bedrock = require('bedrock');
-var database = require('bedrock-mongodb');
+
+// modules
 require('bedrock-express');
+var database = require('bedrock-mongodb');
 
 // custom configuration
 bedrock.config.mongodb.name = 'bedrock_rest_dev'; // default: bedrock_dev
-bedrock.config.mongodb.host = 'localhost';      // default: localhost
-bedrock.config.mongodb.port = 27017;            // default: 27017
+bedrock.config.mongodb.host = 'localhost';        // default: localhost
+bedrock.config.mongodb.port = 27017;              // default: 27017
 bedrock.config.mongodb.username = 'bedrock_rest'; // default: bedrock
-bedrock.config.mongodb.password = 'password';   // default: password
+bedrock.config.mongodb.password = 'password';     // default: password
 
 // the mongodb database 'bedrock_rest_dev' and the 'bedrock_rest' user will
 // be created on start up following a prompt for the admin user credentials
@@ -23,24 +24,24 @@ bedrock.events.on('bedrock-mongodb.ready', function(callback) {
 });
 
 bedrock.events.on('bedrock-express.configure.routes', function(app) {
-  app.get('/users', function(req, res){
+  app.get('/people', function(req, res) {
     database.collections.people.find({}).toArray(function(err, docs){
       res.send(docs);
     });
   });
-  
-  app.post('/users/:name', function(req, res){
-    database.collections.people.insert([{name: req.param("name")}],
-     function(err, result){
-      res.send(result.result);
-    });
+
+  app.post('/people/:name', function(req, res) {
+    database.collections.people.insert(
+      [{name: req.param('name')}], function(err, result) {
+        res.send(result.result);
+      });
   });
 
-  app.delete('/users/:name', function(req, res){
-    database.collections.people.remove({name: req.param("name")},
-      function(err, result){
+  app.delete('/people/:name', function(req, res) {
+    database.collections.people.remove(
+      {name: req.param('name')}, function(err, result) {
         res.send(result.result);
-    });
+      });
   });
 });
 
