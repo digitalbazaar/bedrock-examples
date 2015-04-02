bf59dbb787776149258abd3edc35886c295ebedf
Amit Ishairzay <amit.ishairzay@gmail.com>
2015-04-02 17:40:49 -0400

    Add initial alert system and scrap controller system

--- basic-modals-example/example/example.js
+++ basic-modals-example/example/example.js
@@ -13,7 +13,22 @@ define([
 
 'use strict';
 
-var module = angular.module('app.example', []);
+var module = angular.module('app.example', ['bedrock.alert']).directive('alert-directive', showAlert);
+
+/* @ngInject */
+function showAlert(brAlertService){
+  return {
+    restrict: 'E',
+    scope: {},
+    link: function(scope, element) {
+      brAlertService.add('info', 'my-directive loaded.');
+      scope.errorOccurred = function(err) {
+        // add a feedback error; if the scope is destroyed, remove the error
+        brAlertService.add('error', err, {scope: scope});
+      };
+    }
+  };
+}
 
 /* @ngInject */
 module.config(function($routeProvider) {
@@ -24,31 +39,22 @@ module.config(function($routeProvider) {
 });
 
 /* @ngInject */
-module.controller('PeopleController', function($scope) {
-  $scope.people = [];
+module.controller('PeopleController', function() {
+  var self = this;
+  self.people = [];
 });
 
-/* @ngInject */
-module.controller('PersonController', function($scope) {
-  $scope.person = {name:"", traits:[]};
-});
-
-/* @ngInject */
-module.controller('TraitController', function($scope) {
-  $scope.trait = '';
-});
 
 module.directive('personEditor', function(){
   return{
     restrict: 'E',
     require: '^stackable',
     templateUrl: 'person-editor',
-    controller: 'PersonController',
-    controllerAs: 'model',
     link: function(scope, element, attrs, stackable) {
+      scope.person = {name:"", traits:[]};
       scope.ok = function(person) {
         scope.people.push(person);
-        stackable.close(person);
+        stackable.close(null, person);
       };
     }
   };
@@ -59,12 +65,11 @@ module.directive('traitAdder', function(){
     restrict: 'E',
     require: '^stackable',
     templateUrl: 'trait-adder',
-    controller: 'TraitController',
-    controllerAs: 'model',
     link: function(scope, element, attrs, stackable) {
+      scope.trait = '';
       scope.ok = function(trait) {
         scope.person.traits.push(trait);
-        stackable.close(trait);
+        stackable.close(null, trait);
       };
     }
   };
