d9a3b1a4dbefe9221590a79cfc0db1e54c2d86d0
Dave Longley <dlongley@digitalbazaar.com>
2015-04-04 20:01:58 -0400

    Use new `bedrock-requirejs` feature.
    - Custom-configure `example` as a pseudo bower package.

--- basic-modals-example/index.js
+++ basic-modals-example/index.js
@@ -2,32 +2,25 @@ var bedrock = require('bedrock');
 var path = require('path');
 
 require('bedrock-express');
-
 require('bedrock-requirejs');
 require('bedrock-server');
-
 require('bedrock-views');
 
 bedrock.config.views.paths.push(
   path.join(__dirname)
 );
 
-var config = bedrock.config;
-config.requirejs.config.packages.push({
-  name: 'example',
-  main: './example.js',
-  location: '/bower-components/example'
+// add pseudo bower package
+bedrock.config.requirejs.bower.packages.push({
+  path: path.join(__dirname, 'example'),
+  manifest: {
+    name: 'example',
+    moduleType: 'amd',
+    main: './example.js',
+    dependencies: {
+      angular: '~1.3.0'
+    }
+  }
 });
-config.requirejs.optimize.config.packages.push({
-  name: 'example',
-  main: './example.js',
-  location: path.join(__dirname, 'example')
-});
-config.express.static.push({
-  route: '/bower-components/example',
-  path: path.join(__dirname, 'example')
-});
-config.requirejs.autoload.push('example');
-
 
 bedrock.start();
