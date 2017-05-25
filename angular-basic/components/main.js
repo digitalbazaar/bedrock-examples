87b803a58a3bb7099001ca6378a337052fe9cac1
Dave Longley <dlongley@digitalbazaar.com>
2017-05-25 15:46:15 -0400

    Use `setRootModule`.

--- angular-basic/components/main.js
+++ angular-basic/components/main.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2015-2017 Digital Bazaar, Inc. All rights reserved.
  */
 import angular from 'angular';
-import * as bedrock from 'bedrock-angular';
+import {setRootModule} from 'bedrock-angular';
 import HomeComponent from './home-component';
 import ShowNumbersComponent from './show-numbers-component';
 
@@ -12,7 +12,7 @@ const module = angular.module('angular-basic', []);
 module.component('exHome', HomeComponent);
 module.component('exShowNumbers', ShowNumbersComponent);
 
-bedrock.setMainModule(module);
+setRootModule(module);
 
 /* @ngInject */
 module.config($routeProvider => {
