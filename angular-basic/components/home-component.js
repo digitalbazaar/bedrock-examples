56d474cfc4472ec2a1135e14a50732f5f4343a0e
Joel Eiholzer <jeiholzer123@gmail.com>
2017-06-07 10:25:19 -0400

    upgrade more files.

--- angular-basic/components/home-component.js
+++ angular-basic/components/home-component.js
@@ -1,16 +1,10 @@
 /*!
  * Copyright (c) 2015-2016 Digital Bazaar, Inc. All rights reserved.
  */
-define([], function() {
-
-'use strict';
-
-function register(module) {
-  module.component('exHome', {
-    controller: Ctrl,
-    templateUrl: requirejs.toUrl('angular-basic/home-component.html')
-  });
-}
+export default {
+   controller: Ctrl,
+   templateUrl: 'angular-basic/home-component.html'
+};
 
 /* @ngInject */
 function Ctrl() {
@@ -30,8 +24,4 @@ function Ctrl() {
     }
     self.display[showProperty] = true;
   }
-}
-
-return register;
-
-});
+};
--- angular-basic/components/show-numbers-component.js
+++ angular-basic/components/show-numbers-component.js
@@ -1,26 +1,16 @@
 /*!
  * Copyright (c) 2015-2016 Digital Bazaar, Inc. All rights reserved.
  */
-define([], function() {
-
-'use strict';
-
-function register(module) {
-  module.component('exShowNumbers', {
-    bindings: {
-      sortDescending: '<'
-    },
-    controller: Ctrl,
-    templateUrl: requirejs.toUrl('angular-basic/show-numbers-component.html')
-  });
-}
+export default {
+  bindings: {
+    sortDescending: '<'
+  },
+  controller: Ctrl,
+  templateUrl: 'angular-basic/show-numbers-component.html'
+};
 
 /* @ngInject */
 function Ctrl() {
   var self = this;
   self.numbers = [1, 5, 99, 6, 75];
-}
-
-return register;
-
-});
+};
