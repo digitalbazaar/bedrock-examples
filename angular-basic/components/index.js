ac50d29bb6a637ae5e9cbcd30dc4560953a5c544
Joel Eiholzer <jeiholzer123@gmail.com>
2017-06-07 10:15:38 -0400

    add index.js

---
+++ angular-basic/components/index.js
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) 2015-2016 Digital Bazaar, Inc. All rights reserved.
+ */
+import angular from 'angular';
+import * as bedrock from 'bedrock-angular';
+import HomeComponent from './home-component.js';
+import ShowNumbersComponent from './show-numbers-component.js';
+
+var module = angular.module('angular-basic', []);
+
+bedrock.setRootModule(module);
+
+module.component('exHome', HomeComponent);
+module.component('exShowNumbers', ShowNumbersComponent);
+
+/* @ngInject */
+module.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      title: 'Angular Basic Home',
+      template: '<ex-home></ex-home>'
+    });
+});
