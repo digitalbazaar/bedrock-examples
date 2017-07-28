f1858fdebbbfa6094ec3f9bb0946d4a67507a3ce
Dave Longley <dlongley@digitalbazaar.com>
2017-07-28 17:43:34 -0400

    Add WIP some test code.

--- angular-web-request/components/mediator-component.js
+++ angular-web-request/components/mediator-component.js
@@ -3,12 +3,60 @@
  */
 'use strict';
 
+//import * as wrMediator from 'web-request-mediator';
+import * as rpc from 'web-request-rpc';
+
 export default {
   controller: Ctrl,
   templateUrl: 'angular-web-request/mediator-component.html'
 };
 
+let _control;
+
+// class TestManager {
+//   async test() {
+//     return 'test function called';
+//   }
+// }
+const testManager = {
+  test: async () => 'test function called',
+  toggle: async () => {
+    await _control.show();
+    console.log('hiding in 3 seconds...');
+    setTimeout(() => {
+      console.log('hiding.');
+      _control.hide();
+    }, 3000);
+  }
+};
+
 /* @ngInject */
 function Ctrl() {
   const self = this;
+
+  (async () => {
+    const origin = 'https://bedrock.dev:18443';
+    const client = new rpc.Client();
+    const injector = await client.connect(origin);
+    const control = injector.define('core.control', {
+      functions: ['ready', 'show', 'hide']
+    });
+    _control = control;
+
+    const server = new rpc.Server();
+    //server.define('testManager', new TestManager());
+    server.define('testManager', testManager);
+    server.listen(origin);
+
+    control.ready();
+
+    // wrMediator.server.define('testManager', new TestManager());
+
+    // const origin = 'https://bedrock.dev:18443';
+    // wrMediator.listen({
+    //   origin: origin
+    // });
+
+    // wrMediator
+  })();
 }
--- angular-web-request/components/service-provider-component.js
+++ angular-web-request/components/service-provider-component.js
@@ -3,6 +3,8 @@
  */
 'use strict';
 
+import {polyfill} from './test-polyfill';
+
 export default {
   controller: Ctrl,
   templateUrl: 'angular-web-request/service-provider-component.html'
@@ -11,4 +13,19 @@ export default {
 /* @ngInject */
 function Ctrl() {
   const self = this;
+
+  self.loadPolyfill = () => {
+    (async () => {
+      try {
+        await polyfill.load();
+        console.log('calling test manager "test" function...');
+        const result = await polyfill.testManager.test();
+        console.log('result', result);
+        await polyfill.testManager.toggle();
+        console.log('done');
+      } catch(e) {
+        console.error(e);
+      }
+    })();
+  };
 }
