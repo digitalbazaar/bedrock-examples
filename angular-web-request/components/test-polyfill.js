0f1d3912331c72132782a56f0d0bef1f8b0a6761
Dave Longley <dlongley@digitalbazaar.com>
2017-08-02 13:27:46 -0400

    Simplify test-polyfill.

--- angular-web-request/components/test-polyfill.js
+++ angular-web-request/components/test-polyfill.js
@@ -3,28 +3,15 @@
  */
 'use strict';
 
-//import {WebRequestMediatorClient} from 'web-request-mediator-client';
 import * as rpc from 'web-request-rpc';
 
 export const polyfill = {};
 
-let injector;
-
-const testManagerDefinition = {
-  functions: ['test', 'toggle']
-};
-
 polyfill.load = async () => {
-  //const url = '/mediator';
-
   const url = 'https://bedrock.dev:18443/mediator';
-  console.log('server context', rpc);
   const serverContext = new rpc.ServerContext();
   const injector = await serverContext.createWindow(url);
-  polyfill.testManager = injector.define('testManager', testManagerDefinition);
-
-  // const client = new WebRequestMediatorClient();
-  // injector = await client.load(url);
-  // injector.define('testManager', testManagerDefinition);
-  // api.testManager = await injector.get('paymentManager');
+  polyfill.testManager = injector.define('testManager', {
+    functions: ['test', 'toggle']
+  });
 };
