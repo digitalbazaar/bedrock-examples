2ea8647e410601862ebe1316f0eb2d9ae2eaaa53
Matthew Collier <collier.matthew@gmail.com>
2017-11-01 16:30:23 -0400

    Add simple example.

--- mongodb-minimal/lib/index.js
+++ mongodb-minimal/lib/index.js
@@ -12,24 +12,63 @@ require('./config');
 // open some collections once the database is ready
 bedrock.events.on('bedrock-mongodb.ready', callback => async.auto({
   openCollections: callback =>
-    database.openCollections(['mongodb_minimal'], callback)
+    database.openCollections(['mongodb_minimal'], callback),
+  removeCollection: ['openCollections', (results, callback) =>
+    database.collections.mongodb_minimal.remove(callback)]
 }, err => callback(err)));
 
+const docs = [
+  {'origin': 'a', 'destination': 'b'},
+  {'origin': 'b', 'destination': 'c'},
+  {'origin': 'c', 'destination': ['d', 'e', 'g']},
+  {'origin': 'd', 'destination': 'e'},
+  {'origin': 'e', 'destination': 'f'},
+  {'origin': 'f', 'destination': 'g'},
+  {'origin': 'g', 'destination': 'h'},
+  // {'origin': 'h', 'destination': 'i'},
+  // {'origin': 'a', 'destination': 'e'},
+];
+// const docs = [
+//   {'origin': 'a', 'destination': 'b'},
+//   {'origin': 'b', 'destination': 'c'},
+//   {'origin': 'c', 'destination': 'd'},
+//   {'origin': 'd', 'destination': 'e'},
+//   {'origin': 'e', 'destination': 'f'},
+//   {'origin': 'f', 'destination': 'g'},
+//   {'origin': 'g', 'destination': 'h'},
+//   {'origin': 'h', 'destination': 'i'},
+//   {'origin': 'a', 'destination': 'e'},
+// ];
+
 bedrock.events.on('bedrock.started', () => {
-  const databaseEntryId = uuid();
-  const databaseEntryData = 'This will work';
-  const query = {databaseEntryId};
   async.auto({
-    insert: callback =>
-      database.collections.mongodb_minimal.insert({databaseEntryId,
-        databaseEntryData}, callback),
-    find: ['insert', (results, callback) => database.collections.mongodb_minimal
-      .find(query).toArray((err, result) => {
+    insert: callback => async.each(docs, (d, callback) =>
+      database.collections.mongodb_minimal.insert(d, callback), callback),
+    agregate: ['insert', (results, callback) => database.collections
+      .mongodb_minimal.aggregate([
+        {$match: {origin: 'a'}},
+        {
+          $graphLookup: {
+            from: 'mongodb_minimal',
+            startWith: '$destination',
+            connectFromField: 'destination',
+            connectToField: 'origin',
+            depthField: 'numConnections',
+            as: 'traverseroute',
+            restrictSearchWithMatch: {'destination': {'$ne': 'h'}}
+          }
+        },
+        // {$project: {'_id': 0, 'origin': 1, 'traverseroute': 1}},
+        // FIXME: sort doesn't seem to have any effect before/after project
+        // {$sort: {'traverseroute.numConnections': 1}},
+      ]).toArray((err, result) => {
         console.log('LOCAL-FIND', JSON.stringify(result, null, 2));
         callback();
       })]
   }, err => {
-    console.log('An error occurred', err);
+    if(err) {
+      console.log('ERROR', err);
+    }
     bedrock.exit(err);
   });
 });
