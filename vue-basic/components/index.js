cf2c505aa49c4c075d7944e3aab8b748b154c5e5
Dave Longley <dlongley@digitalbazaar.com>
2018-04-05 17:20:03 -0400

    Use `brVue` name.

--- vue-basic/components/index.js
+++ vue-basic/components/index.js
@@ -1,15 +1,15 @@
 /*!
  * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
  */
-import * as bedrock from 'bedrock-vue';
+import * as brVue from 'bedrock-vue';
 import Home from './Home.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 // install all plugins
-Vue.use(bedrock);
+Vue.use(brVue);
 
-bedrock.setRootVue(() => {
+brVue.setRootVue(() => {
   const router = new VueRouter({
     mode: 'history',
     routes: [
